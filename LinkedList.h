5f72e1a5685c1f129e520b44a7abc6e80eb80233
Inozpavel <Inozpavel@mail.ru>
2020-08-07 00:11:13 +0300

    Рефакторинг кода

--- LinkedList.h
+++ LinkedList.h
@@ -17,11 +17,9 @@ class LinkedList
 	/* Структура связного списка:
 	head -> node_1 -> ... -> node_n -> nullptr
 	head так же node, но самый первый
-	node указывающий на nullptr называется хвостом (tail)
-	Linked List (связный список) будем называть просто списком или LL
-	LL[0] = head
-	LL[1] = head.next
-	LL[2] = head.next.next
+	Linked List[0] = head
+	Linked List[1] = head.next
+	Linked List[2] = head.next.next
 	*/
 private:
 	//Узел списка, содержит данные и указатель на следующий элемент
@@ -38,7 +36,7 @@ private:
 
 	// Удаляет элемент, сохраняя целосность списка
 	// previous = nullptr для удаления головного объекта
-	void remove_node(Node* node, Node* previous = nullptr)
+	void _remove_node(Node* node, Node* previous = nullptr)
 	{
 		if (node == nullptr) return;
 		if (previous != nullptr)
@@ -47,7 +45,7 @@ private:
 			_head = node->next;
 		delete node;
 	}
-	// Ищет элемент с начаал списка по индексу
+	// Ищет элемент с начала списка по индексу
 	// В стучае успеха возвращает указатель на него, иначе nullptr
 	Node* _get_element(unsigned index)
 	{
@@ -63,26 +61,26 @@ private:
 		return current;
 	}
 	// Получает предыдущий Node
+	// Возвращает указатель на него в случае успеха, иначе nullptr
 	Node* _get_previous(Node* node)
 	{
-		Node* current = node;
-		Node* previous = nullptr;
-		for (Node* startPos = _head;
-			startPos != nullptr;
-			startPos = startPos->next)
+		Node* previousElement = nullptr;
+		for (Node* startElement = _head;
+			startElement != nullptr;
+			startElement = startElement->next)
 		{
-			if (startPos == current)
-				return previous;
-			previous = startPos;
+			if (startElement == node)
+				return previousElement;
+			previousElement = startElement;
 		}
 		return nullptr;
 	}
 	// Ищет Node, данные в которой равны value
 	// Возвращает указатель на него в случае успеха, иначе nullptr
-	Node* _search(T value, Node* startElem = nullptr)
+	Node* _search(T value, Node* startElement = nullptr)
 	{
-		Node* current = startElem;
-		if (startElem == nullptr)
+		Node* current = startElement;
+		if (startElement == nullptr)
 			current = _head;
 		for (; current != nullptr; current = current->next)
 			if (current->data == value)
@@ -95,6 +93,8 @@ public:
 	LinkedList() : _head(nullptr), _nodesCount(0)
 	{
 	}
+
+#pragma region Adding elements
 
 	// Добавление элемента на указанную позицию
 	void insert(unsigned pos, T element)
@@ -114,43 +114,48 @@ public:
 		_nodesCount++;
 	}
 
-	// Добавление элемента в конец
+	// Добавление элемента в конец списка
 	void push_back(T element)
 	{
 		insert(_nodesCount, element);
 	}
 
-	// Добавление элемента в начало
+	// Добавление элемента в начало	списка
 	void push_front(T element)
 	{
 		insert(0, element);
 	}
 
-	// Удаление одного элемента по значению value
-	// Вернёт true в случае успеха (нашёл)
+#pragma endregion		 
+
+#pragma region Methods remove
+
+	// Удаление одного (первого) элемента по значению value
+	// Вернёт true в случае успешного удаления, иначе false
 	bool remove(T value)
 	{
 		Node* foundElem = _search(value);
 		if (foundElem == nullptr)
 			return false;
-		remove_node(foundElem, _get_previous(foundElem));
+		_remove_node(foundElem, _get_previous(foundElem));
 		_nodesCount--;
 		return true;
 	}
 
 	// Удаление элемента на позиции pos
+	// Вернёт true в случае успешного удаления, иначе false
 	bool remove_at(unsigned pos)
 	{
 		if (pos >= _nodesCount)
 			return false;
 		if (pos == 0)
-			remove_node(_head);
+			_remove_node(_head);
 		else
 		{
 			Node* previous = _get_element(pos - 1);
 			if (previous == nullptr)
 				return false;
-			remove_node(previous->next, previous);
+			_remove_node(previous->next, previous);
 		}
 		_nodesCount--;
 		return true;
@@ -166,8 +171,12 @@ public:
 		return count;
 	}
 
+#pragma endregion
+
+#pragma region Methods find
+
 	// Поиск одного элемента по значению
-	// В случае успеха вернёт позицию первого элемента
+	// В случае успеха вернёт позицию первого элемента, иначе константу NOT_FOUND
 	unsigned find(T value, unsigned start_pos = 0)
 	{
 		for (Node* current = _get_element(start_pos);
@@ -175,11 +184,11 @@ public:
 			current = current->next, start_pos++)
 			if (current->data == value)
 				return start_pos;
-		return ~0; // unsigned_max
+		return NOT_FOUND;
 	}
 
 	// Поиск всех элементов со значением value
-	// Вернёт список позиций найденных элементов
+	// Вернёт список (vector<unsigned>) всех позиций найденных элементов
 	vector<unsigned> find_all(T value)
 	{
 		vector<unsigned> founds = {};
@@ -187,13 +196,19 @@ public:
 		do
 		{
 			i = find(value, i);
-			if (i != ~0)
+			if (i != NOT_FOUND)
 				founds.push_back(i);
 			else
 				break;
 		} while (i++ < _nodesCount);
 		return founds;
 	}
+
+#pragma endregion
+
+#pragma region Methods update
+
+
 	// Изменяет значение первого найденного элемента old_value на new_value 
 	bool update(T old_value, T new_value)
 	{
@@ -219,19 +234,23 @@ public:
 		return i;
 	}
 
-	// Обмен местами элементов first и second
-	void swap(unsigned first_pos, unsigned second_pos)
+#pragma endregion
+
+#pragma region Useful methods
+
+	// Обмен местами элементов двух элементов
+	void swap(unsigned firstIndex, unsigned secondIndex)
 	{
 		if (_nodesCount == 0)
 			throw out_of_range("Ошибка! Список пуст!");
-		if (first_pos >= _nodesCount || second_pos >= _nodesCount)
+		if (firstIndex >= _nodesCount || secondIndex >= _nodesCount)
 			throw out_of_range("Ошибка! Некорректный индекс!");
-		T data = _get_element(first_pos)->data;
-		_get_element(first_pos)->data = _get_element(second_pos)->data;
-		_get_element(second_pos)->data = data;
+		T data = _get_element(firstIndex)->data;
+		_get_element(firstIndex)->data = _get_element(secondIndex)->data;
+		_get_element(secondIndex)->data = data;
 	}
 
-	// Вычисляет длину списка (или возвращает кэшированную)
+	// Возвращает количество элементов в списке
 	unsigned size()
 	{
 		return _nodesCount;
@@ -260,15 +279,23 @@ public:
 			cout << endl;
 	}
 
-	// Удаляет все элементы из [start; stop]
-	bool erase(unsigned start, unsigned stop)
+#pragma endregion
+
+#pragma region Methods erase clear
+
+	// Удаляет все элементы из диапазона [start..stop]
+	// stopIndex индекс включается в диапазон
+	void erase(unsigned startIndex, unsigned stopIndex)
 	{
-		for (; start < stop; stop--)
+		if (_nodesCount == 0)
+			return;
+		if (stopIndex >= _nodesCount)
+			throw out_of_range("Ошибка! Некорректно задан индекс!");
+		while (startIndex <= stopIndex)
 		{
-			if (!remove_at(start))
-				return false;
+			remove_at(startIndex);
+			stopIndex--;
 		}
-		return true;
 	}
 
 	// Удаляет все элементы начиная со start
@@ -284,11 +311,13 @@ public:
 	}
 
 	// Очистка памяти, удаление списка
-	// NotImplemented
 	~LinkedList()
 	{
 		clear();
 	}
+
+#pragma endregion
+
 };
 
 #endif
